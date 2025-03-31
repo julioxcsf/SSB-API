2d03eff92f281dfb7683e0e93660771d53587a28
julioxcsf <julioxcsf@poli.ufrj.br>
2025-03-31 04:01:45 -0300

    remoções pontuais da injeção de classe não usadas

--- src/main/java/com/meuprojeto/banco/sistemabancario/service/AccountService.java
+++ src/main/java/com/meuprojeto/banco/sistemabancario/service/AccountService.java
@@ -5,15 +5,16 @@ import com.meuprojeto.banco.sistemabancario.model.*;
 import com.meuprojeto.banco.sistemabancario.repository.AccountRepository;
 import com.meuprojeto.banco.sistemabancario.repository.ClientRepository;
 import com.meuprojeto.banco.sistemabancario.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@RequiredArgsConstructor
 @Service
 public class AccountService {
     private final AccountRepository accountRepository;
-    private final ClientRepository clientRepository;
-    private final TransactionRepository transactionRepository;
+
     private final Random random = new Random();
 
     public Long gerarNumeroContaUnico() {
@@ -22,14 +23,6 @@ public class AccountService {
             numero = 10000L + random.nextInt(90000); // número entre 10000 e 99999
         } while (accountRepository.existsByNumber(numero));
         return numero;
-    }
-
-    public AccountService(AccountRepository accountRepository,
-                          ClientRepository clientRepository,
-                          TransactionRepository transactionRepository) {
-        this.accountRepository = accountRepository;
-        this.clientRepository = clientRepository;
-        this.transactionRepository = transactionRepository;
     }
 
     public Account criarConta(Account account) {
